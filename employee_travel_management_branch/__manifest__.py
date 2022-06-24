053f848b5c65b2ab9ec8b356d1b704da1c964821
Yowit-mbathily <44680721+optesis-khk@users.noreply.github.com>
2022-06-24 13:08:56 +0000

    add employe_travel module

---
+++ employee_travel_management_branch/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Travel Request Branch',
+    'author': 'OPTESIS SA',
+    'version': '14.0.0.0',
+    'category': 'Accounting',
+    'description': """
+Ce module permet de gérer les budgets dans plusieurs branches
+""",
+    'summary': 'Comptabilite',
+    'sequence': 9,
+    "depends" : ['base', 'branch','employee_travel_managment'],
+    "data": [
+        'security/travel_request_ir_rule.xml',
+        'views/travel_request.xml',
+        'views/hr_employee.xml',
+    ],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ employee_travel_management_branch/models/hr_employee.py
@@ -0,0 +1,25 @@
+from odoo import api, fields, models, _, tools
+from odoo.exceptions import UserError
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    @api.model 
+    def default_get(self, field): 
+        result = super(HrEmployee, self).default_get(field)
+        user_obj = self.env['res.users']
+        branch_id = user_obj.browse(self.env.user.id).branch_id.id
+        result['branch_id'] = branch_id
+        return result
+
+    branch_id = fields.Many2one('res.branch', string='Branch')
+
+    @api.onchange('branch_id')
+    def _onchange_branch_id(self):
+        for obj in self:
+            selected_brach = obj.branch_id
+            if selected_brach:
+                user_id = self.env['res.users'].browse(self.env.uid)
+                user_branch = user_id.sudo().branch_id
+                if user_branch and user_branch.id != selected_brach.id:
+                    raise UserError("Please select active branch only. Other may create the Multi branch issue. \n\ne.g: If you wish to add other branch then Switch branch from the header and set that.")
