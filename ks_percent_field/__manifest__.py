7d03261c642da13b981927b9b14cb1152597acdb
Yowit-mbathily <44680721+optesis-khk@users.noreply.github.com>
2022-07-18 17:09:51 +0000

    [ADD] app ks_percent_field

---
+++ ks_percent_field/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': "Percent Field",
+
+    'summary': """
+        Percent Field v14.0""",
+
+    'description': """
+              Percent field for odoo 14.0. 
+      """,
+    'author': "Ksolves India Ltd.",
+    'website': "https://www.ksolves.com/",
+    'license': 'LGPL-3',
+    'currency': 'EUR',
+    'price': '0.0',
+    'live_test_url': 'https://youtu.be/Guuyj7Gns00',
+    'category': 'tools',
+    'support': 'sales@ksolves.com',
+    'version': '14.0.1.0.0',
+    'images': [
+        'static/description/main.jpg',
+    ],
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'web'],
+
+    # always loaded
+    'data': [
+        'views/assets.xml',
+
+    ],
+}
+# -*- coding: utf-8 -*-
